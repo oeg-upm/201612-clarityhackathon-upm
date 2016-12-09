f403a783613c9247932abbb225ba8ed466d1104b
Freddy Priyatna <freddy.priyatna@gmail.com>
2016-12-09 13:45:33 +0100

    updates eGov src java files

--- eGov/src/main/java/oeg/clarity/egov/tramites/TramiteManager.java
+++ eGov/src/main/java/oeg/clarity/egov/tramites/TramiteManager.java
@@ -5,41 +5,45 @@ import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.w3c.dom.Document;
+
+import oeg.clarity.egov.parsers.Utility;
 
 /**
  *
  * @author admin
  */
 public class TramiteManager {
-    
-    public static String result = "";
-    
-    public static void test()
-    {
-	if (result.isEmpty())
-        {
-            init();
-        }
-    }
-    
-    
-    public static void init(){
-	ClassLoader classLoader = TramiteManager.class.getClassLoader();
-	try {
-	    result = IOUtils.toString(classLoader.getResourceAsStream("data/tramite-corrected.jsonld"));
-            JSONParser parser = new JSONParser();
-            JSONObject jobj = (JSONObject) parser.parse(result);
-            JSONArray arr = (JSONArray) jobj.get("result");
-            int tam = arr.size();
-            for(int i=0;i<tam;i++)
-            {
-                JSONObject jtramite = (JSONObject) arr.get(i);
-                String title = (String) jtramite.get("title");
-                System.out.println(title);
-            }
-            
-	} catch (Exception e) {
-		e.printStackTrace();
-	}        
-    }
+
+	public static String result = "";
+
+	public static void test()
+	{
+		if (result.isEmpty())
+		{
+			init();
+		}
+	}
+
+
+	public static void init(){
+		ClassLoader classLoader = TramiteManager.class.getClassLoader();
+		try {
+			result = IOUtils.toString(classLoader.getResourceAsStream("data/tramite-corrected.jsonld"));
+			JSONParser parser = new JSONParser();
+			JSONObject jobj = (JSONObject) parser.parse(result);
+			JSONArray arr = (JSONArray) jobj.get("result");
+			int tam = arr.size();
+			for(int i=0;i<tam;i++)
+			{
+				JSONObject jtramite = (JSONObject) arr.get(i);
+				String title = (String) jtramite.get("title");
+				System.out.println(title);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}        
+	}
+	
 }
